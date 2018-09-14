165f0ba5114342ec36f9f2e1fb8579c4769b1f82
Matt Pitt <matt.ianpitt@gmail.com>
2018-09-14 14:20:32 +0200

    set up basic game

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
-  def home
-  end
+  def home; end
+
+  def first_first; end
+
+  def first_second; end
+
+  def explode; end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'first_first', to: 'pages#first_first', as: :first_first
+  get 'first_second', to: 'pages#first_second', as: :first_second
+  get 'explode', to: 'pages#explode', as: :explode
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
